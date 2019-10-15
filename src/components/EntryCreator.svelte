8691ac353b9b3bc08410679958171bd1280c6a45
VenkateshA <venkatesha@pdinet.com>
2019-10-15 14:41:32 +0530

    Added quotes changes in separate component - Added "Quote of the Day" - Need to have store for "quote of the day" #5

--- src/components/EntryCreator.svelte
+++ src/components/EntryCreator.svelte
@@ -1,7 +1,6 @@
 <script>
 	let db;
     let request = indexedDB.open("GratefulDiary", 2);
-    let randomQuote = '';
     let currentEntryBody,
         currentEntryTitle;
     const currentEntryTimestamp = Math.floor(Date.now() / 1000);
@@ -61,18 +60,7 @@
         currentEntryTitle = null;
     }
 
-    function DayQuote(){
-        var arrQuote = ["Your time is limited, so don't waste it living someone else's life",
-        "Everyone lives by selling something", 
-        "If you cannot do great things, do small things in a great way",
-        "Nothing is really work unless you would rather be doing something else",
-        "Without hustle, talent will only carry you so far"];
-
-        //var num = Math.floor(Math.random() * arrQuote.length);
-        randomQuote = arrQuote[Math.floor(Math.random()*Math.floor(arrQuote.length))];
-    }
-
-    DayQuote()
+    
  </script>
 
  <style>
@@ -85,12 +73,7 @@
         text-align: right;
     }
 
-    .form-quote{
-        text-align: center;
-        padding-top:50px;
-        color:#777;
-        font-size:28px;
-    }
+    
  </style>
 
 <div class="form-container">
@@ -99,6 +82,4 @@
     <button on:click={handleNewEntry}>Save</button>
     <button on:click={clearEntry}>Clear</button>
 </div>
-<div class="form-quote">
-    <p>" {randomQuote} "</p>
-</div>
+
